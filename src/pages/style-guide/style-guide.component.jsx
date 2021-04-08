8b3c42d5b299605d52bf906d823376b243c0b0fb
selceeus <matt@carbonbasedcreative.com>
2021-04-08 17:17:21 -0600

    Added StyleGuide to Page components

---
+++ src/pages/style-guide/style-guide.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style-guide.styles.scss';
+
+const StyleGuide = props => {
+    return(
+        <section className='style-guide'>
+            <h1>Test</h1>
+        </section>
+    );
+}
+
+export default StyleGuide;
