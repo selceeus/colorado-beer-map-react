eae6fa9d3b16c28bfc1b80da351b8debd7074bd8
selceeus <matt@carbonbasedcreative.com>
2021-05-31 17:58:36 -0600

    Minor changes and restructuring

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ class App extends Component {
                 )} />
               </main>
           {/*<Footer />*/}
-          <StyleGuide />
+          {/*<StyleGuide />*/}
         </div>
       );
     }
